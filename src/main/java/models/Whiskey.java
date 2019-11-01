d9c6db3619bdfef7e763d6a465f96d96db31aeaa
deltagphys <cty.jgates@gmail.com>
2019-11-01 08:48:35 -0400

    obj fields

--- src/main/java/models/Whiskey.java
+++ src/main/java/models/Whiskey.java
@@ -1,4 +1,13 @@
 package models;
 
 public class Whiskey {
+
+    private float volume;
+    private String brand;
+    private int id;
+    private String name;
+    private int qty;
+    private float price;
+
+    
 }
